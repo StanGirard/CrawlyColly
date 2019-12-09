28af66bf0ea2b72fac0372b26f24762be232c471
Stanislas Girard <stan@Beast.local>
2019-12-09 16:12:30 +0100

    Removed commentary

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	website := "www.lemonde.fr"
+	website := "www.devlup.fr"
 	crawled := crawlAll(website, getAllSitemap(website))
-	uniqueCrawled :=  unique(crawled)
+	uniqueCrawled := unique(crawled)
 	fmt.Println("Visited ", len(uniqueCrawled), " pages on ", website)
-	//crawled := crawl(website, "https://www.lemonde.fr/sitemap_index.xml")
-	
 	write_to_file(crawled, website)
 }
